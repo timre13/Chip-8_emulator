5ce27a1a6926fc4ec932b98b8b8b7d08eba76309
timre13 <itorteli13@gmail.com>
2020-05-04 16:30:05 +0200

    Support for beep on win32

--- sound.cpp
+++ sound.cpp
@@ -1,14 +1,22 @@
 #include "sound.h"
 
-#include <iostream>
-#include "ncurses.h"
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    #include <utilapiset.h>
+#else
+    #include <iostream>
+    #include "ncurses.h"
+#endif
 
 void Sound::makeBeepSound()
 {
-    if (beep()) // Try to beep using ncurses.
-    {
-        // If failed, try using the \a character.
-        std::cout << '\a';
-        std::cout.flush();
-    }
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+        Beep(4000, 100);
+    #else
+        if (beep()) // Try to beep using ncurses.
+        {
+            // If failed, try using the \a character.
+            std::cout << '\a';
+            std::cout.flush();
+        }
+    #endif
 }
